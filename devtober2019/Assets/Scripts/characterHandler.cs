df834e6291a2d27c7cbfc940618a5b70941b8c99
ReluctantPirate <reluctantpirategames@gmail.com>
2019-10-03 22:54:25 -0400

    just the barest of beginnings
    a hyper basic character controller, doesn't do anything really

---
+++ devtober2019/Assets/Scripts/characterHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterHandler : MonoBehaviour
+{
+    [Header("Hookups")]
+    public GameObject[] perches;
+    public GameObject buttonRow;
+    public int currentPerch;
+
+    [Header("Character Status")]
+    public int energyRemaining;
+    public float defensePoints;
+    public int woundPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = perches[currentPerch].transform.position;
+    }
+
+    public void advanceCharacter()
+    {
+        if(currentPerch + 1 < perches.Length)//there are still places to advance to
+        {
+            currentPerch++;
+            energyRemaining--;
+        }
+    }
+
+    public void retreatCharacter()
+    {
+        if(currentPerch > 0)//there are still places to reatreat to
+        {
+            currentPerch--;
+            energyRemaining--;
+        }
+    }
+
+    public void declareAttack(int type)
+    {
+
+    }
+}
